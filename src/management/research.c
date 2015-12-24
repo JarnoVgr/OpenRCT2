e92c962f5172e75e65e18bbce5a7287e392017aa
IntelOrca <ted@brambles.org>
2015-12-24 13:37:48 +0000

    add notification conditions

--- src/management/research.c
+++ src/management/research.c
@@ -19,6 +19,7 @@
  *****************************************************************************/
 
 #include "../addresses.h"
+#include "../config.h"
 #include "../game.h"
 #include "../interface/window.h"
 #include "../localisation/date.h"
@@ -202,8 +203,11 @@ void research_finish_item(sint32 entryIndex)
 		if (RCT2_GLOBAL(0x009AC06C, uint8) == 0) {
 			RCT2_GLOBAL(RCT2_ADDRESS_COMMON_FORMAT_ARGS, rct_string_id) = ((rideEntry->flags & RIDE_ENTRY_FLAG_SEPARATE_RIDE_NAME)) ?
 				rideEntry->name : base_ride_type + 2;
-			if (!gSilentResearch)
-				news_item_add_to_queue(NEWS_ITEM_RESEARCH, 2249, entryIndex);
+			if (!gSilentResearch) {
+				if (gConfigNotifications.ride_researched) {
+					news_item_add_to_queue(NEWS_ITEM_RESEARCH, 2249, entryIndex);
+				}
+			}
 		}
 
 		research_invalidate_related_windows();
@@ -218,8 +222,11 @@ void research_finish_item(sint32 entryIndex)
 		// I don't think 0x009AC06C is ever not 0, so probably redundant
 		if (RCT2_GLOBAL(0x009AC06C, uint8) == 0) {
 			RCT2_GLOBAL(RCT2_ADDRESS_COMMON_FORMAT_ARGS, rct_string_id) = scenerySetEntry->name;
-			if (!gSilentResearch)
-				news_item_add_to_queue(NEWS_ITEM_RESEARCH, 2250, entryIndex);
+			if (!gSilentResearch) {
+				if (gConfigNotifications.ride_researched) {
+					news_item_add_to_queue(NEWS_ITEM_RESEARCH, 2250, entryIndex);
+				}
+			}
 		}
 
 		research_invalidate_related_windows();
